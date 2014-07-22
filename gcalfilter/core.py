5f8e343a742319fd3787f653aae306a126604d4e
Matt Kangas <matt.kangas@mongodb.com>
2014-07-22 10:30:16 -0400

    PARSE ALL THE DATES
    major reformatting of report too

--- gcalfilter/core.py
+++ gcalfilter/core.py
@@ -1,7 +1,8 @@
 import datetime
-import requests
+import itertools
 import pprint
 import pymongo
+import requests
 import time
 import urllib
 import urlparse
@@ -102,43 +103,137 @@ def is_user_in_team(user, team):
 def entry_creator(e):
     return e['creator']['email']
 
+
 def entry_title(e):
     return e['title']
 
-def entry_when(e):
-    # FIXME normalize
-    start = e['when'][0]["start"]
-    end = e['when'][0]["end"]
-    return (start, end)
 
-def entry_sort_by_date(entries):
-    return sorted(entries, key=lambda e: entry_when(e)[0])
+def parse_date(datestr):
+    datestr = datestr.split("T")[0]
+    fmt = r"%Y-%m-%d"
+    return datetime.datetime.strptime(datestr, fmt).date()
+
+
+def entry_add_dateset(entry):
+    ds = Dateset()
+    for when in entry['when']:
+        ds_e = Dateset.from_span(parse_date(when['start']),
+                                 parse_date(when['end'])
+                    )
+        ds.update(ds_e)
+
+    entry['dateset'] = ds
+    return entry
+
 
 def filter_calendar_by_team(calendar, team):
     return (entry for entry in calendar
             if is_user_in_team(entry_creator(entry), team))
 
 
-def run_report_plaintext(db, team, calendar):
-    # FIXME: add daterange parameter
+def entries_group_by_weeks(raw_entries, num_weeks):
+    entries = [entry_add_dateset(e) for e in raw_entries]
+    weeks = next_weeks(num_weeks)
 
-    vacations = db.cal[calendar].find()
-    report = ""
+    ## Cartesian product (weeks X entries), reduce by week
+    week_events = [(w, e)
+                   for w in weeks for e in entries
+                   if not w.isdisjoint( e['dateset'] )]
+
+    select_0 = lambda x: x[0]
+    grouped = itertools.groupby(week_events, select_0)
     
-    entries = filter_calendar_by_team(vacations, team)
+    for (week, igroup) in grouped:
+        events = [g[1] for g in igroup]
 
-    for entry in entry_sort_by_date(entries):
-        report += plaintext_report_entry(entry, team)
-        report += '\n'
+        ## Keep only dates in this week
+        ## FIXME: dedup multiple events from same user
+        z = [(week.intersection( e['dateset'] ), e) for e in events]
+        z = sorted(z, key=lambda x: min(x[0]))
+
+        yield (week, z)
+
+
+def run_report_plaintext(db, team, calendar, num_weeks=8):
+
+    all_entries = db.cal[calendar].find()
+    team_entries = filter_calendar_by_team(all_entries, team)
+    grouped = entries_group_by_weeks(team_entries, num_weeks)
+
+    select_0 = lambda x: x[0]
+
+    report = ""
+    for (week, entries) in grouped:
+        report += "\n"
+        report += "==== Week of: %s ============================\n" % \
+            week.prettyweek()
+        report += "\n"
+
+        for (dates, entry) in entries:
+            report += plaintext_report_entry(entry, team, dates)
+            report += '\n'
 
     return report
 
-def plaintext_report_entry(e, team):
+def plaintext_report_entry(e, team, dates):
     email = entry_creator(e)
     name = _team_cache[team][email]
-    out  = "%s\n\t%s\n\t%s" % (name, entry_title(e), entry_when(e))
+    out  = "%s\n\t%s\n\t%s" % (name, entry_title(e), dates)
     return out
 
+
+def next_weeks(num_weeks):
+    """Return n Datespans representing the next n weeks"""
+    today = datetime.date.today()
+    oneweek = datetime.timedelta(7)
+    oneday = datetime.timedelta(1)
+
+    monday = today - datetime.timedelta(today.weekday())
+    weeks = []
+    for n in range(0, num_weeks):
+        next_monday = monday + oneweek
+        weeks.append( Dateset.from_span(monday, next_monday) )
+        monday = next_monday
+    return weeks
+
+class Dateset(set):
+    @classmethod
+    def from_span(cls, datefrom, dateto):
+        """Initialize set from date range"""
+        assert isinstance(datefrom, datetime.date)
+        assert isinstance(dateto, datetime.date)
+        oneday = datetime.timedelta(1)
+
+        days = []
+        day = datefrom
+        while day < dateto:
+            days.append(day)
+            day += oneday
+        return cls(days)
+
+    def __str__(self):
+        fmt = r"%a %b %d"
+        fmt_y = r"%a %b %d %Y"
+
+        d_min = min(self)
+        d_max = max(self)
+        delta = d_max - d_min
+        contiguous = len(self) == delta.days + 1
+
+        if len(self) == 1:
+            return d_min.strftime(fmt_y)
+        elif len(self) > 2 and contiguous:
+            return '%s thru %s (%d days)' % \
+            (d_min.strftime(fmt), d_max.strftime(fmt_y),
+             len(self))
+        else:
+            days = [d.strftime(fmt) for d in sorted(self)]
+            return "%s (%d days)" % (", ".join(days), len(self))
+
+    def prettyweek(self):
+        """Variant of __str__ for explicit weeks"""
+        fmt_y = r"%a %b %d %Y"
+        return "%s" % (min(self).strftime(fmt_y))
 
 ## --------------------------------------------------------
 
